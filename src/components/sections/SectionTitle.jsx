6d82588603b8ceaa0eaf5019ae5169b20f14dc45
Njörd <davidbillamboz@gmail.com>
2019-07-11 22:35:22 +0200

    Add font-weight bold to section title

--- src/components/sections/SectionTitle.jsx
+++ src/components/sections/SectionTitle.jsx
@@ -4,6 +4,7 @@ const Title = styled.h1`
   text-transform: uppercase;
   color: ${props => props.theme.color3};
   font-size: 1.7rem;
+  font-weight: bold;
 
   @media (min-width: ${props => props.theme.breakpointTablet}) {
     font-size: 2rem;
