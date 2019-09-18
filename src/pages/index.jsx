79dda6c04bc83bab89da00c7c810bf3927d21fe1
Njörd <davidbillamboz@gmail.com>
2019-09-18 12:56:43 +0200

    Add Digital Climate Stripe plugin

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -29,6 +29,7 @@ const IndexPage = ({ data }) => (
       <Helmet>
         <title>{data.site.siteMetadata.title}</title>
         <meta name="description" content={data.site.siteMetadata.description} />
+        <script src="https://assets.digitalclimatestrike.net/widget.js" async />
       </Helmet>
 
       <NavBar />
